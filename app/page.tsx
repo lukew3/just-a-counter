c81fc6a4bfcade31cfa36b602b3ff799b34b21d5
Luke Weiler <lukew25073@gmail.com>
2024-09-02 17:34:35 -0400

    first ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,48 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from "react";
+import { SourceTextModule } from "vm";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div>
+      <h2>Just A Counter</h2>
+      <Counters />
+    </div>
   );
+}
+
+function Counters() {
+  return <div className={styles.counters}>
+    <Counter 
+      name="Pushups"
+    />
+  </div>
+}
+
+function Counter(props: any) {
+  const [count, setCount] = useState(0);
+
+  const decCounter = () => {
+    const decVal : number = Number(window.prompt("Decrement by:"));
+    console.log(decVal);
+    setCount((prevCount) => prevCount - decVal);
+  }
+
+  const incCounter = () => {
+    const incVal : number = Number(window.prompt("Increment by:"));
+    console.log(incVal);
+    console.log(count + incVal)
+    setCount((prevCount) => prevCount + incVal);
+  }
+
+  return <div>
+    <h4>{props.name} (daily)</h4>
+    <div className={styles.counterBody}>
+      <div className={styles.counterButton} onClick={decCounter}>-n</div>
+      <p className={styles.counterVal} onClick={() => setCount((prevCount) => prevCount + 1)}>{count}</p>
+      <div className={styles.counterButton} onClick={incCounter}>+n</div>
+    </div>
+  </div>
 }
